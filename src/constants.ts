9a970712a7db1651dfe0f70458a10c9411a112c0
Wen Xia <wen.xia@wework.com>
2021-10-13 18:37:35 +0800

    feat: v2.0

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
 // CLI 配置文件名
-export const FTINTL_CONFIG_FILENAME = ".ftintlrc";
+export const FTINTL_CONFIG_FILENAME = '.ftintlrc';
 // 中文文件名
-export const DEFAULT_ZHHANS = "zh-hans";
+export const DEFAULT_ZHHANS = 'zh-hans';
 // 资源文件目录
-export const DEFAULT_LANG_DIR = "./.fastIntl/";
+export const DEFAULT_LANG_DIR = './.fastIntl/';
 
 // CLI 配置文件
 export const DEFAULT_CLI_CONFIG_FILE = {
@@ -14,7 +14,13 @@ export const DEFAULT_CLI_CONFIG_FILE = {
   // 可选语言
   langs: [DEFAULT_ZHHANS],
   // 忽略的文件
-  ignoreFile: () => false,
+  // @ts-ignore
+  ignoreFile: (basename) => {
+    return /\.(json|md|png|jpg|jpeg|svg)$/.test(basename);
+  },
   // 忽略的文件夹
-  ignoreDir: () => false,
+  // @ts-ignore
+  ignoreDir: (basename) => {
+    return /\.(node_modules|__tests__)$/.test(basename);
+  },
 };
