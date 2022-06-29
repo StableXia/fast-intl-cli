de00c9f8e7bee914829d848c24cdab74b4884bd8
Wen Xia <wen.xia@wework.cn>
2022-06-29 11:13:30 +0800

    fix: bugs

--- src/constants.ts
+++ src/constants.ts
@@ -13,6 +13,7 @@ export const DEFAULT_FAST_INTL_CONFIG_FILE = {
     langDir: '${DEFAULT_LANG_DIR}',
     defaultLang: '${DEFAULT_LANG}',
     langs: ['${DEFAULT_LANG}'],
+    importI18N: 'import I18N from 'src/utils/I18N';',
     ignoreFile: (basename) => {
       return /\.(json|md|png|jpg|jpeg|svg)$/.test(basename);
     },
@@ -24,6 +25,7 @@ export const DEFAULT_FAST_INTL_CONFIG_FILE = {
     langDir: '${DEFAULT_LANG_DIR}',
     defaultLang: '${DEFAULT_LANG}',
     langs: ['${DEFAULT_LANG}'],
+    importI18N: 'import I18N from 'src/utils/I18N';',
     ignoreFile: (basename: string) => {
       return /\.(json|md|png|jpg|jpeg|svg)$/.test(basename);
     },
